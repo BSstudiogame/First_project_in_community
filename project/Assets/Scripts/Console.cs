d5000e3425132eaccb145a13e29d42759e672a14
Oxnack <128256361+Oxnack@users.noreply.github.com>
2023-12-17 14:15:21 +0300

    Update Console.cs

--- project/Assets/Scripts/Console.cs
+++ project/Assets/Scripts/Console.cs
@@ -7,7 +7,9 @@ public class Console : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+              //         Hello
+
+
     }
 
     // Update is called once per frame
